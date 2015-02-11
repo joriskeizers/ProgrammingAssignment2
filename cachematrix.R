e4d9edb1c9bdfacb9ad5a39027faaacbc227d2a7
JorisKeizers <joriskeizers@hotmail.com>
2015-02-11 23:16:37 +0100

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are based upon the example shown
+## by R. Deng in the preparations for the assignment
 
-## Write a short comment describing this function
+## This first function calculates - given a matrix x -
+## the inverse matrix of x
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## ensure that the matrix m is empty
+  m <- NULL ## will contain the result
+  set <- function(y) {
+        x <<- y
+        m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve 
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
 
+## The second matrix checks is the inverse matrix of x
+## had been calculated before. If so, the cached inverse matrix
+## is returned. Otherwise, it is being calculated
+
+CacheInverse <- function(x,...) {
+  m <-x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)    
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setsolve(m)
+  m
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
